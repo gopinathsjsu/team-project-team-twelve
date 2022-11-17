8cb5780f3d3c48639e9bf0bdeb79b9dc200a4494
MidnightCastle <poojapatel.3010@gmail.com>
2022-11-16 20:43:31 -0800

    added mio_terminal retrieve update and delete apis using GenericAPI

--- backend/AMS/jwtapp/views.py
+++ backend/AMS/jwtapp/views.py
@@ -227,7 +227,22 @@ class Terminal_create(APIView):
         except Exception as e:
             return Response({"msg":serializer.errors,"status":status.HTTP_400_BAD_REQUEST})
 
+class Terminal_RUD(RetrieveModelMixin,UpdateModelMixin,DestroyModelMixin,GenericAPIView):
 
+    queryset=Mio_terminal.objects.all()
+    serializer_class=MioTerminalSerializer
+
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
+    
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
+
+    def patch(self, request, *args, **kwargs):
+        return self.partial_update(request, *args, **kwargs)
+
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
 
 
 class AllTerminalGatesInfo(ListAPIView):

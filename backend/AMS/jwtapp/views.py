2483b211ce8344c792599c0af07ecee6e897ae27
MidnightCastle <poojapatel.3010@gmail.com>
2022-11-16 20:32:37 -0800

    added mio_airline retrieve update and delete apis using GenericAPI

--- backend/AMS/jwtapp/views.py
+++ backend/AMS/jwtapp/views.py
@@ -171,6 +171,21 @@ class Airline_create(APIView):
         except Exception:
             return Response({'message': 'invalid input ','status': 400})
 
+class Airline_RUD(RetrieveModelMixin,UpdateModelMixin,DestroyModelMixin,CreateModelMixin,GenericAPIView):
+    queryset=Mio_airline.objects.all()
+    serializer_class=MioAirlineSerializer
+
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
+    
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
+
+    def patch(self, request, *args, **kwargs):
+        return self.partial_update(request, *args, **kwargs)
+
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
 
 
 

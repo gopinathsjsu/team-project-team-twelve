25d647ca5cde5bf240e2eb65bd52e9e10f224a80
zeelvs <zeelvasant.satasiya@sjsu.edu>
2022-11-21 14:40:49 -0800

    added arrival and app component

---
+++ frontend/src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import Arrival from './Arrival';
+// import Departure from './Departure';
+import './App.css';
+
+
+function App() {
+  return (  
+    <div className = "A-D-Panel">
+    <Arrival />
+    {/* <Departure /> */}
+    {// <div className="App">
+    //   <header className="App-header">
+    //     <p>
+    //       Airport Management System
+    //     </p>
+    //     <button>SignIn</button>
+    //   </header>
+    // </div>
+  }
+    </div> 
+  );
+}
+
+export default App;

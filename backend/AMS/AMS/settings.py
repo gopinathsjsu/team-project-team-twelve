ad3296b22f28762270d00bccb75497b6151738a4
“swapnilwagh2204” <swapnilswagh1234@gmail.com>
2022-12-02 17:22:18 -0800

    cors fixed

--- backend/AMS/AMS/settings.py
+++ backend/AMS/AMS/settings.py
@@ -50,10 +50,12 @@ INSTALLED_APPS = [
 
 ]
 
+
+
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -61,6 +63,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+CORS_ORIGIN_ALLOW_ALL = True
 ROOT_URLCONF = 'AMS.urls'
 
 TEMPLATES = [

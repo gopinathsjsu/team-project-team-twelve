9a27b107b40ec89fd77d81ce8c0dd6025d42cc2a
MidnightCastle <poojapatel.3010@gmail.com>
2022-12-04 21:28:11 -0800

    latest db schema

--- backend/db_scripts/mio_airline.sql
+++ backend/db_scripts/mio_airline.sql
@@ -16,13 +16,13 @@ VALUES('AAL_AA2405', 'AAL', 'AA2405', 'American Airlines', true);
 INSERT INTO public.jwtapp_mio_airline (airline_flight_key, airline_code_id, flight_code, airline_name, is_available)
 VALUES('SWA_WN5011', 'SWA', 'WN5011', 'Southwest Airlines', true);
 INSERT INTO public.jwtapp_mio_airline (airline_flight_key, airline_code_id, flight_code, airline_name, is_available)
-VALUES('SWA_WN5012', 'SWA', 'WN5012', 'American Airlines', true);
+VALUES('SWA_WN5012', 'SWA', 'WN5012', 'Southwest Airlines', true);
 INSERT INTO public.jwtapp_mio_airline (airline_flight_key, airline_code_id, flight_code, airline_name, is_available)
-VALUES('SWA_WN5013', 'SWA', 'WN5013', 'American Airlines', true);
+VALUES('SWA_WN5013', 'SWA', 'WN5013', 'Southwest Airlines', true);
 INSERT INTO public.jwtapp_mio_airline (airline_flight_key, airline_code_id, flight_code, airline_name, is_available)
-VALUES('SWA_WN5014', 'SWA', 'WN5014', 'American Airlines', true);
+VALUES('SWA_WN5014', 'SWA', 'WN5014', 'Southwest Airlines', true);
 INSERT INTO public.jwtapp_mio_airline (airline_flight_key, airline_code_id, flight_code, airline_name, is_available)
-VALUES('SWA_WN5015', 'SWA', 'WN5015', 'American Airlines', true);
+VALUES('SWA_WN5015', 'SWA', 'WN5015', 'Southwest Airlines', true);
 
 
 INSERT INTO public.jwtapp_mio_airline (airline_flight_key, airline_code_id, flight_code, airline_name, is_available)
@@ -34,7 +34,7 @@ VALUES('SIA_SQ3203', 'SIA', 'SQ3203', 'Singapore Airlines', true);
 INSERT INTO public.jwtapp_mio_airline (airline_flight_key, airline_code_id, flight_code, airline_name, is_available)
 VALUES('SIA_SQ3204', 'SIA', 'SQ3204', 'Singapore Airlines', true);
 INSERT INTO public.jwtapp_mio_airline (airline_flight_key, airline_code_id, flight_code, airline_name, is_available)
-VALUES('SIA_SQ3205', 'SIA', 'SQ3205', 'Singapore Airlines', true)
+VALUES('SIA_SQ3205', 'SIA', 'SQ3205', 'Singapore Airlines', true);
 
 
 INSERT INTO public.jwtapp_mio_airline (airline_flight_key, airline_code_id, flight_code, airline_name, is_available)
@@ -48,5 +48,16 @@ VALUES('DAL_DL8008', 'DAL', 'DL8008', 'Delta Air Lines', true);
 INSERT INTO public.jwtapp_mio_airline (airline_flight_key, airline_code_id, flight_code, airline_name, is_available)
 VALUES('DAL_DL8009', 'DAL', 'DL8009', 'Delta Air Lines', true);
 
+
+INSERT INTO public.jwtapp_mio_airline (airline_flight_key, airline_code_id, flight_code, airline_name, is_available)
+VALUES('BAW_BA7001', 'BAW', 'BA7001', 'British Airways', true);
+INSERT INTO public.jwtapp_mio_airline (airline_flight_key, airline_code_id, flight_code, airline_name, is_available)
+VALUES('BAW_BA7002', 'BAW', 'BA7002', 'British Airways', true);
+INSERT INTO public.jwtapp_mio_airline (airline_flight_key, airline_code_id, flight_code, airline_name, is_available)
+VALUES('BAW_BA7003', 'BAW', 'BA7003', 'British Airways', true);
+INSERT INTO public.jwtapp_mio_airline (airline_flight_key, airline_code_id, flight_code, airline_name, is_available)
+VALUES('BAW_BA7004', 'BAW', 'BA7004', 'British Airways', true);
+INSERT INTO public.jwtapp_mio_airline (airline_flight_key, airline_code_id, flight_code, airline_name, is_available)
+VALUES('BAW_BA7005', 'BAW', 'BA7005', 'British Airways', true);
 
 
--- backend/db_scripts/mio_terminal_gate.sql
+++ backend/db_scripts/mio_terminal_gate.sql
@@ -1,38 +1,38 @@
 
 --delete from jwtapp_mio_terminal;
 
-INSERT INTO public.jwtapp_mio_terminal (terminal_gate, gate_status) VALUES('A1_G1', 'open');
-INSERT INTO public.jwtapp_mio_terminal (terminal_gate, gate_status) VALUES('A1_G2', 'open');
-INSERT INTO public.jwtapp_mio_terminal (terminal_gate, gate_status) VALUES('A1_G3', 'open');
-INSERT INTO public.jwtapp_mio_terminal (terminal_gate, gate_status) VALUES('A1_G4', 'open');
-INSERT INTO public.jwtapp_mio_terminal (terminal_gate, gate_status) VALUES('A1_G5', 'open');
-INSERT INTO public.jwtapp_mio_terminal (terminal_gate, gate_status) VALUES('A1_G6', 'open');
-INSERT INTO public.jwtapp_mio_terminal (terminal_gate, gate_status) VALUES('A1_G7', 'open');
-INSERT INTO public.jwtapp_mio_terminal (terminal_gate, gate_status) VALUES('A1_G8', 'open');
-INSERT INTO public.jwtapp_mio_terminal (terminal_gate, gate_status) VALUES('A1_G9', 'open');
-INSERT INTO public.jwtapp_mio_terminal (terminal_gate, gate_status) VALUES('A1_G10', 'open');
+INSERT INTO public.jwtapp_mio_terminal (terminal_gate, gate_status) VALUES('T1_G1', 'open');
+INSERT INTO public.jwtapp_mio_terminal (terminal_gate, gate_status) VALUES('T1_G2', 'open');
+INSERT INTO public.jwtapp_mio_terminal (terminal_gate, gate_status) VALUES('T1_G3', 'open');
+INSERT INTO public.jwtapp_mio_terminal (terminal_gate, gate_status) VALUES('T1_G4', 'open');
+INSERT INTO public.jwtapp_mio_terminal (terminal_gate, gate_status) VALUES('T1_G5', 'open');
+INSERT INTO public.jwtapp_mio_terminal (terminal_gate, gate_status) VALUES('T1_G6', 'open');
+INSERT INTO public.jwtapp_mio_terminal (terminal_gate, gate_status) VALUES('T1_G7', 'open');
+INSERT INTO public.jwtapp_mio_terminal (terminal_gate, gate_status) VALUES('T1_G8', 'open');
+INSERT INTO public.jwtapp_mio_terminal (terminal_gate, gate_status) VALUES('T1_G9', 'open');
+INSERT INTO public.jwtapp_mio_terminal (terminal_gate, gate_status) VALUES('T1_G10', 'open');
 
-INSERT INTO public.jwtapp_mio_terminal (terminal_gate, gate_status) VALUES('A2_G1', 'open');
-INSERT INTO public.jwtapp_mio_terminal (terminal_gate, gate_status) VALUES('A2_G2', 'open');
-INSERT INTO public.jwtapp_mio_terminal (terminal_gate, gate_status) VALUES('A2_G3', 'open');
-INSERT INTO public.jwtapp_mio_terminal (terminal_gate, gate_status) VALUES('A2_G4', 'open');
-INSERT INTO public.jwtapp_mio_terminal (terminal_gate, gate_status) VALUES('A2_G5', 'open');
-INSERT INTO public.jwtapp_mio_terminal (terminal_gate, gate_status) VALUES('A2_G6', 'open');
-INSERT INTO public.jwtapp_mio_terminal (terminal_gate, gate_status) VALUES('A2_G7', 'open');
-INSERT INTO public.jwtapp_mio_terminal (terminal_gate, gate_status) VALUES('A2_G8', 'open');
-INSERT INTO public.jwtapp_mio_terminal (terminal_gate, gate_status) VALUES('A2_G9', 'open');
-INSERT INTO public.jwtapp_mio_terminal (terminal_gate, gate_status) VALUES('A2_G10', 'open');
+INSERT INTO public.jwtapp_mio_terminal (terminal_gate, gate_status) VALUES('T2_G1', 'open');
+INSERT INTO public.jwtapp_mio_terminal (terminal_gate, gate_status) VALUES('T2_G2', 'open');
+INSERT INTO public.jwtapp_mio_terminal (terminal_gate, gate_status) VALUES('T2_G3', 'open');
+INSERT INTO public.jwtapp_mio_terminal (terminal_gate, gate_status) VALUES('T2_G4', 'open');
+INSERT INTO public.jwtapp_mio_terminal (terminal_gate, gate_status) VALUES('T2_G5', 'open');
+INSERT INTO public.jwtapp_mio_terminal (terminal_gate, gate_status) VALUES('T2_G6', 'open');
+INSERT INTO public.jwtapp_mio_terminal (terminal_gate, gate_status) VALUES('T2_G7', 'open');
+INSERT INTO public.jwtapp_mio_terminal (terminal_gate, gate_status) VALUES('T2_G8', 'open');
+INSERT INTO public.jwtapp_mio_terminal (terminal_gate, gate_status) VALUES('T2_G9', 'open');
+INSERT INTO public.jwtapp_mio_terminal (terminal_gate, gate_status) VALUES('T2_G10', 'open');
 
-INSERT INTO public.jwtapp_mio_terminal (terminal_gate, gate_status) VALUES('A3_G1', 'open');
-INSERT INTO public.jwtapp_mio_terminal (terminal_gate, gate_status) VALUES('A3_G2', 'open');
-INSERT INTO public.jwtapp_mio_terminal (terminal_gate, gate_status) VALUES('A3_G3', 'open');
-INSERT INTO public.jwtapp_mio_terminal (terminal_gate, gate_status) VALUES('A3_G4', 'open');
-INSERT INTO public.jwtapp_mio_terminal (terminal_gate, gate_status) VALUES('A3_G5', 'open');
-INSERT INTO public.jwtapp_mio_terminal (terminal_gate, gate_status) VALUES('A3_G6', 'open');
-INSERT INTO public.jwtapp_mio_terminal (terminal_gate, gate_status) VALUES('A3_G7', 'open');
-INSERT INTO public.jwtapp_mio_terminal (terminal_gate, gate_status) VALUES('A3_G8', 'open');
-INSERT INTO public.jwtapp_mio_terminal (terminal_gate, gate_status) VALUES('A3_G9', 'open');
-INSERT INTO public.jwtapp_mio_terminal (terminal_gate, gate_status) VALUES('A3_G10', 'open');
+INSERT INTO public.jwtapp_mio_terminal (terminal_gate, gate_status) VALUES('T3_G1', 'open');
+INSERT INTO public.jwtapp_mio_terminal (terminal_gate, gate_status) VALUES('T3_G2', 'open');
+INSERT INTO public.jwtapp_mio_terminal (terminal_gate, gate_status) VALUES('T3_G3', 'open');
+INSERT INTO public.jwtapp_mio_terminal (terminal_gate, gate_status) VALUES('T3_G4', 'open');
+INSERT INTO public.jwtapp_mio_terminal (terminal_gate, gate_status) VALUES('T3_G5', 'open');
+INSERT INTO public.jwtapp_mio_terminal (terminal_gate, gate_status) VALUES('T3_G6', 'open');
+INSERT INTO public.jwtapp_mio_terminal (terminal_gate, gate_status) VALUES('T3_G7', 'open');
+INSERT INTO public.jwtapp_mio_terminal (terminal_gate, gate_status) VALUES('T3_G8', 'open');
+INSERT INTO public.jwtapp_mio_terminal (terminal_gate, gate_status) VALUES('T3_G9', 'open');
+INSERT INTO public.jwtapp_mio_terminal (terminal_gate, gate_status) VALUES('T3_G10', 'open');
 
 
 
